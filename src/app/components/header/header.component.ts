04e3b4364d2fcdf05607e8c0be232e635e5d065f
Ricardo Belfort <rbelfort2004@gmail.com>
2024-09-18 19:08:00 -0400

    feat: criando estrutura e hero section

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  selectedLanguage = 'br';
+  dropdownOpen = false;
+
+  options = [
+    { value: 'en', text: 'English', img: 'united-states.png' },
+    { value: 'es', text: 'Spanish', img: 'spain.png' },
+    { value: 'br', text: 'Portuguese', img: 'brazil.png' },
+  ];
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectOption(option: any) {
+    this.selectedLanguage = option.value;
+    this.dropdownOpen = false;
+  }
+
+  getSelectedLanguageImage() {
+    const selectedOption = this.options.find(
+      (option) => option.value === this.selectedLanguage
+    );
+    return selectedOption ? selectedOption.img : '';
+  }
+}
