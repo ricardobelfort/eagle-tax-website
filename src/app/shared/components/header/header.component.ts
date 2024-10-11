f4b01608493beca6e388dfe3121d0709b58bd129
Ricardo Belfort <rbelfort2004@gmail.com>
2024-10-11 10:00:48 -0400

    fix: ajustes na versão do projeto e no dropdown de linguagens

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
@@ -17,7 +17,11 @@ export class HeaderComponent {
   languages = [
     { value: 'en', label: 'English', image: 'us-flag.png' },
     { value: 'es', label: 'Spanish', image: 'spain-flag.png' },
-    { value: 'br', label: 'Portuguese', image: 'brazil-flag.png' },
+    {
+      value: 'br',
+      label: 'Português',
+      image: 'brazil-flag.png',
+    },
   ];
 
   toggleDropdown() {
@@ -25,19 +29,32 @@ export class HeaderComponent {
   }
 
   selectLanguage(language: string) {
-    this.selectedLanguage = language;
-    this.dropdownOpen = false;
+    if (this.selectedLanguage !== language) {
+      this.selectedLanguage = language;
+    }
+    setTimeout(() => {
+      this.dropdownOpen = false;
+    }, 100);
   }
 
   getSelectedLanguageImage() {
     const selected = this.languages.find(
       (lang) => lang.value === this.selectedLanguage
     );
-    return selected ? selected.image : '';
+    return selected ? 'assets/images/' + selected.image : '';
   }
 
   onLanguageChange(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     this.selectedLanguage = selectElement.value;
+  }
+
+  // Captura clique fora do dropdown para fechar
+  @HostListener('document:click', ['$event'])
+  closeDropdownOnClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.custom-select-wrapper')) {
+      this.dropdownOpen = false;
+    }
   }
 }
